70d40d62034c6b7d49d20ceb9980ee8d02291c88
KalleH92 <kalle.hagberg92@hotmail.com>
2023-10-08 15:20:29 +0200

    Add files via upload

--- Dice_Roller_Game/src/com/Kalle/game/Main.java
+++ Dice_Roller_Game/src/com/Kalle/game/Main.java
@@ -1,6 +1,11 @@
 package com.Kalle.game;
 
 public class Main {
+        public static void main(String[] args) {
 
-        System.out.println("Input number");
+
+                int hello = 56;
+                System.out.println("Input number");
+                System.out.println(hello);
+        }
 }
